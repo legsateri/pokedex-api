4155703a9775cc78a1a475aa0914ae7d7e660182
Allegra Pusateri <allegrapusateri@gmail.com>
2019-08-05 20:16:32 -0500

    set up types and pokemon endpoints

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
-console.log(process.env.API_TOKEN)
+const POKEDEX = require('./pokedex.json')
 
 const app = express()
 app.use(morgan('dev'))
 
-const validTypes =  [`Bug`, `Dark`, `Dragon`, `Electric`, 
-                    `Fairy`, `Fighting`, `Fire`, `Flying`, 
-                    `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, 
-                    `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
-
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-                    
-    console.log('validate bearer token middleware')
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unautorized request' })
@@ -25,17 +17,32 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-function handleGetTypes(req, res) {
+const validTypes =  [`Bug`, `Dark`, `Dragon`, `Electric`, 
+                    `Fairy`, `Fighting`, `Fire`, `Flying`, 
+                    `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, 
+                    `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.get('/types', function handleGetTypes(req, res) {
     res.json(validTypes)
-}
+})
 
-app.get('/types', handleGetTypes)
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
+    if (req.query.name) {
+        response = response.filter(pokemon =>
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        )
+    }
 
-app.get('/pokemon', handleGetPokemon)
+    if (req.query.type) {
+        response = response.filter(pokemon => 
+            pokemon.type.includes(req.query.type)
+        )
+    }
+
+    res.json(response)
+})
 
 const PORT = 8000
 
