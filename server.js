38b93d9a8e61390973457419396d4a4584bd85f4
Allegra Pusateri <allegrapusateri@gmail.com>
2019-08-06 20:23:00 -0500

    prepare server for deployment

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const POKEDEX = require('./pokedex.json')
 
 const app = express()
-app.use(morgan('dev'))
+const morganSetting  = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
@@ -44,7 +50,17 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
